93f4d3fdf1edee01a9d9f25a90f8d231777748a9
joshvernon <jvernon_483@hotmail.com>
2020-01-13 23:47:43 -0600

    Add JailActivity.Driver.download_report/1

--- lib/jail_activity/driver.ex
+++ lib/jail_activity/driver.ex
@@ -23,4 +23,10 @@ defmodule JailActivity.Driver do
 
     {:ok, links}
   end
+
+  def download_report(link) do
+    url = to_charlist(link)
+    {:ok, {{'HTTP/1.1', 200, 'OK'}, _headers, body}} = :httpc.request(:get, {url, []}, [], [])
+    {:ok, body}
+  end
 end
