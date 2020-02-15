9a54f6071e5dc56f33f9fabe970abc7731aff969
joshvernon <jvernon_483@hotmail.com>
2020-02-15 12:47:36 -0600

    Move download_report/1 to a separate Downloader module

---
+++ lib/dowloader.ex
@@ -0,0 +1,7 @@
+defmodule JailActivity.Downloader do
+  def download_report(link) do
+    url = to_charlist(link)
+    {:ok, {{'HTTP/1.1', 200, 'OK'}, _headers, body}} = :httpc.request(:get, {url, []}, [], [])
+    {:ok, body}
+  end
+end
--- lib/jail_activity/collector.ex
+++ lib/jail_activity/collector.ex
@@ -23,10 +23,4 @@ defmodule JailActivity.Collector do
 
     {:ok, links}
   end
-
-  def download_report(link) do
-    url = to_charlist(link)
-    {:ok, {{'HTTP/1.1', 200, 'OK'}, _headers, body}} = :httpc.request(:get, {url, []}, [], [])
-    {:ok, body}
-  end
 end
