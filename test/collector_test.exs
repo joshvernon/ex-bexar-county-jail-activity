9b0a4ebc14256d8fdf3195d2458351d341cb924f
joshvernon <jvernon_483@hotmail.com>
2020-10-03 17:05:51 -0500

    Alias the Collector module

--- test/collector_test.exs
+++ test/collector_test.exs
@@ -1,9 +1,11 @@
 defmodule CollectorTest do
   use ExUnit.Case, async: true
 
+  alias JailActivity.Scrape.Collector
+
   @tag :selenium
   test "there are seven links" do
-    {:ok, links} = JailActivity.Scrape.Collector.get_report_links()
+    {:ok, links} = Collector.get_report_links()
     assert length(links) == 7
   end
 end
