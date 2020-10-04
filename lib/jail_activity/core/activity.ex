eee9f6d51a04840618c44c0ee0f729c257300766
joshvernon <jvernon_483@hotmail.com>
2020-10-04 00:24:16 -0500

    Make struct definitions more readable

--- lib/jail_activity/core/activity.ex
+++ lib/jail_activity/core/activity.ex
@@ -1,10 +1,12 @@
 defmodule JailActivity.Core.Activity do
-  defstruct booking_number: nil,
-            booking_time: nil,
-            defendant_race: nil,
-            defendant_sex: nil,
-            time: nil,
-            charges: []
+  defstruct [
+    :booking_number,
+    :booking_time,
+    :defendant_race,
+    :defendant_sex,
+    :time,
+    charges: []
+  ]
 
   def new(fields) do
     struct!(__MODULE__, fields)
